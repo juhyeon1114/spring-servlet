3e609815f9cabcf610a0c4705404cccc5d891523
Kai Kim <juhyeon1114@gmail.com>
2022-11-19 17:44:09 +0900

    log : v5

--- src/main/java/hello/springservlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/springservlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -5,7 +5,11 @@ import hello.springservlet.web.frontcontroller.MyView;
 import hello.springservlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.springservlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.springservlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.springservlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.springservlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.springservlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.springservlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.springservlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdaptor;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,13 +34,19 @@ public class FrontControllerServletV5 extends HttpServlet {
     }
 
     private void initHandlerAdapters() {
+
         handlerAdaptors.add(new ControllerV3HandlerAdapter());
+        handlerAdaptors.add(new ControllerV4HandlerAdaptor());
     }
 
     private void initHandlerMappingMap() {
         handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
         handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
 
     @Override
